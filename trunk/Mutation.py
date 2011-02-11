5261018096c9f2e86b072cdab1c88e21fa7ebae2
alfaceor <alfaceor@24d41815-4987-a127-74ec-3e213f12f83a>
2011-02-11 23:53:33 +0000

    Mutation.py : field country added to the class FindMutations.py : small testing changes

--- trunk/Mutation.py
+++ trunk/Mutation.py
@@ -1,9 +1,10 @@
 class Mutation:
-   def __init__(self, wildtype_AN, secuence_AN, old_aa, position_1D, new_aa):
+   def __init__(self, wildtype_AN, secuence_AN, old_aa, position_1D, new_aa,country):
       self.wildtype_AN=wildtype_AN
       self.secuence_AN=secuence_AN
       self.old_aa=old_aa
       self.position_1D=position_1D
       self.new_aa=new_aa
+      self.country=country
    def __repr__(self):
-      return repr((self, wildtype_AN, secuence_AN, old_aa, position_1D, new_aa))
+      return repr((self, wildtype_AN, secuence_AN, old_aa, position_1D, new_aa,country))
