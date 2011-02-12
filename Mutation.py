44fb94a3511e6a5936e85bbbf6aa890273955c5f
alfaceor <alfaceor@24d41815-4987-a127-74ec-3e213f12f83a>
2011-02-12 16:49:27 +0000

    Csv file generation, with secondary structure and active site regions

--- Mutation.py
+++ Mutation.py
@@ -1,10 +1,10 @@
 class Mutation:
-   def __init__(self, wildtype_AN, secuence_AN, old_aa, position_1D, new_aa,country):
+   def __init__(self, wildtype_AN, sequence_AN, old_aa, position_1D, new_aa,country):
       self.wildtype_AN=wildtype_AN
-      self.secuence_AN=secuence_AN
+      self.sequence_AN=sequence_AN
       self.old_aa=old_aa
       self.position_1D=position_1D
       self.new_aa=new_aa
       self.country=country
    def __repr__(self):
-      return repr((self.wildtype_AN, self.secuence_AN, self.old_aa, self.position_1D, self.new_aa, self.country))
+      return repr((self.wildtype_AN, self.sequence_AN, self.old_aa, self.position_1D, self.new_aa, self.country))
